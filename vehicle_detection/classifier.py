26374aa2680e6dea0afe35fcbf8ef6e0a28e4ee9
Matt Shannon <dmshannon@gmail.com>
2017-12-31 16:07:49 -0800

    Added additional feature strategies

--- vehicle_detection/classifier.py
+++ vehicle_detection/classifier.py
@@ -5,6 +5,7 @@ import glob
 import logging as log
 import os
 
+import cv2
 import numpy as np
 import progressbar
 from skimage.feature import hog
@@ -18,6 +19,7 @@ from vehicle_detection.image import convert_color, load_img
 COLOR_SPACE = 'RGB'
 SPATIAL_SIZE = (32, 32)
 HIST_BINS = 32
+BINS_RANGE = (0, 256)
 ORIENTATIONS = 9
 PIX_PER_CELL = 8
 CELLS_PER_BLOCK = 2
@@ -25,20 +27,44 @@ HOG_CHANNEL = None
 
 def dir_to_array(path):
     return glob.glob(path)
-    
+
+def hog_features_all(img):
+    features = []
+    for channel in range(img.shape[2]):
+        features.append(hog_features_by_channel(img, channel))
+
+    return np.ravel(features)
+
+
 def hog_features_by_channel(img, channel=0):
     return hog(
-        img[:, :, channel], 
-        orientations=ORIENTATIONS, 
-        pixels_per_cell=(PIX_PER_CELL, PIX_PER_CELL), 
+        img[:, :, channel],
+        orientations=ORIENTATIONS,
+        pixels_per_cell=(PIX_PER_CELL, PIX_PER_CELL),
         cells_per_block=(CELLS_PER_BLOCK, CELLS_PER_BLOCK),
         transform_sqrt=True,
         feature_vector=True,
         block_norm='L2-Hys',
     )
 
+def spatial_features(img):
+    return cv2.resize(img, SPATIAL_SIZE).ravel()
+
+def color_histogram_features(img):
+    histograms = []
+    for channel in [0, 1, 2]:
+        histograms.append(np.histogram(
+            img[:, :, channel],
+            bins=HIST_BINS,
+            range=BINS_RANGE)[0]
+        )
+
+    return np.concatenate(histograms)
+
 FEATURE_STRATEGIES = [
-    hog_features_by_channel
+    hog_features_all,
+    spatial_features,
+    color_histogram_features
 ]
 
 class Classifier:
@@ -79,9 +105,9 @@ class Classifier:
 
         for path in self.NOT_CAR_SET:
             not_cars.extend(dir_to_array(self._build_data_path(path)))
-        
+
         return cars, not_cars
-        
+
     def _extract_features(self, img_paths):
         features = []
 
@@ -95,7 +121,7 @@ class Classifier:
                 file_features.append(strategy(image))
 
             features.append(np.concatenate(file_features))
-        
+
         return features
 
     def train(self):
@@ -103,10 +129,10 @@ class Classifier:
 
         cars, not_cars = self._get_dataset()
 
-        log.info(f'Extracting vehicles features (set of {len(cars)}')
+        log.info(f'Extracting vehicles features (set of {len(cars)})')
         car_features = self._extract_features(cars)
 
-        log.info(f'Extracting non-vehicles features (set of {len(not_cars)}')
+        log.info(f'Extracting non-vehicles features (set of {len(not_cars)})')
         not_car_features = self._extract_features(not_cars)
 
         X = np.vstack((car_features, not_car_features)).astype(np.float64)
@@ -117,7 +143,7 @@ class Classifier:
         log.debug('Shuffling training data')
         X_train, X_test, y_train, y_test = train_test_split(
             scaled_X, y, test_size=0.2
-        )        
+        )
 
         log.info('Fitting training data')
         self.classifier.fit(X_train, y_train)
@@ -128,7 +154,7 @@ class Classifier:
         return self.classifier.predict(val)
 
     def to_pickle(self, path):
-        # using this as a guide: 
+        # using this as a guide:
         # http://scikit-learn.org/stable/modules/model_persistence.html
         joblib.dump(self.classifier, path)
 
